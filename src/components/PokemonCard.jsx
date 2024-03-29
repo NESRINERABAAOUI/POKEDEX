a8960e994541777f0ddd5d2e72aab05acacbf524
NESRINERABAAOUI <161383698+NESRINERABAAOUI@users.noreply.github.com>
2024-03-29 22:38:03 +0100

    components.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 const PokemonCard = () => {
+
+
   return (
-    <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-      
-      <figcaption>Bulbasaur</figcaption>
-    </figure>
+    pokemonList.map((pokemon) =>(
+        <figure>
+            {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
+    ))    
   )
 }
 
