dd3c4a03bd363267129184dc4fb54b8535b9f2f1
NESRINERABAAOUI <161383698+NESRINERABAAOUI@users.noreply.github.com>
2024-03-29 21:38:42 +0100

    init branch

---
+++ src/PokemonCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+]
+
+export default PokemonCard;
